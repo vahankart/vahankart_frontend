faf0c9c3ee7ffa988eb68150df2690df12669361
kailash360 <kailashkejriwal21@gmail.com>
2021-10-24 18:34:50 +0530

    Used styled components in slogan

--- src/components/Navbar/Slogan/Slogan.jsx
+++ src/components/Navbar/Slogan/Slogan.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import './Slogan.css'
+import styled from 'styled-components'
+import {makeStyles} from "@mui/styles"
+
+const useStyles = makeStyles({
+    slogan:{
+        width: '100%',
+        backgroundColor: 'black',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }
+})
+
 
 function Slogan() {
+
+    const classes = useStyles()
+
+    const H2 = styled.h2`
+        color: ${props => props.red? "red" : "white"};
+        margin-block-start:10px;
+        margin-block-end:10px;
+    `
     return (
-        <div className="slogan">
-                <h2>Cart se Car Tak.</h2>     
-                <h2>Free Delivery and Installation</h2>    
+        <div className={classes.slogan}>
+            <H2 red>Cart se Car Tak.</H2>     
+            <H2>Free Delivery and Installation</H2>    
         </div>
     )
 }
