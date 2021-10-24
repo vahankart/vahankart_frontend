6faff40e9319a2723bc9d4f80a945cc2d97e6c76
kailash360 <kailashkejriwal21@gmail.com>
2021-10-24 22:08:02 +0530

    Added searchbar and logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import Slogan from './Slogan/Slogan'
-import './Navbar.css'
+import styled from 'styled-components'
+import {makeStyles} from "@mui/styles"
+import {ReactComponent as Logo} from './assets/Logo.svg'
+import Search from './Search/Search'
+
+const useStyles = makeStyles({
+    navbarBottom:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-evenly",
+        alignItems:"center",
+        paddingTop:"10px",
+    }
+})
+
 
 function Navbar() {
+
+    const classes = useStyles()
+
+    const Navbar = styled.div`
+        width: 100%;
+        position: fixed;
+        top: 0%;
+        display:flex;
+        flex-direction:column;
+    `
+
     return (
-        <div className="navbar">
+        <Navbar className="navbar">
             <Slogan></Slogan>
-        </div>
+            <div className={classes.navbarBottom}>
+                <Logo></Logo>
+                <Search></Search>
+            </div>
+        </Navbar>
     )
 }
 
---
+++ src/components/Navbar/Search/Search.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import MicNoneOutlinedIcon from '@mui/icons-material/MicNoneOutlined';
+import PhotoCameraOutlinedIcon from '@mui/icons-material/PhotoCameraOutlined';
+import Input from '@mui/material/Input';
+import {makeStyles} from '@mui/styles'
+
+const useStyles = makeStyles({
+    input:{
+        width: '90%'
+    },
+    icon:{
+        verticalAlign:"middle",
+        padding:"0px 5px",
+        cursor:"pointer"
+    },
+    scanner:{
+        padding:"5px",
+        borderLeft:"1px solid black"
+    }
+})
+
+
+function Search() {
+
+    const classes = useStyles()
+
+    const Search = styled.div`
+        width:50%;
+        height:45px;
+        padding:3px;
+        border:1px solid black;
+        display:flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    `
+
+    return (
+        <Search>
+            <SearchOutlinedIcon className={classes.icon} ></SearchOutlinedIcon>
+            <Input 
+                className={classes.input} 
+                disableUnderline
+                placeholder={"Search Accessories..."}
+            ></Input>
+            <MicNoneOutlinedIcon className={classes.icon} ></MicNoneOutlinedIcon>
+            <span className={classes.scanner} >
+                <PhotoCameraOutlinedIcon className={classes.icon} ></PhotoCameraOutlinedIcon>
+            </span>
+        </Search>
+    )
+}
+
+export default Search
