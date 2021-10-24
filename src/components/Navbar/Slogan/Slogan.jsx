2edc645c9ba76344731dba4961f987564a336d10
kailash360 <kailashkejriwal21@gmail.com>
2021-10-24 17:43:33 +0530

    Added alogan component

---
+++ src/components/Navbar/Slogan/Slogan.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Slogan.css'
+
+function Slogan() {
+    return (
+        <div className="slogan">
+                <h2>Cart se Car Tak.</h2>     
+                <h2>Free Delivery and Installation</h2>    
+        </div>
+    )
+}
+
+export default Slogan
