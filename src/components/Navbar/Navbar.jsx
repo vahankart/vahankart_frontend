46f961c248a5451a23675b2838d14a399d4e8571
kailash360 <kailashkejriwal21@gmail.com>
2021-10-24 17:43:19 +0530

    Created navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Slogan from './Slogan/Slogan'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <Slogan></Slogan>
+        </div>
+    )
+}
+
+export default Navbar
