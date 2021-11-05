b1cea69d8c67872725e89a9f7307ec7b58d957c3
kailash360 <kailashkejriwal21@gmail.com>
2021-11-05 15:02:52 +0530

    Added breadcrumb

---
+++ src/components/ProductDetails/Breadcrumb/BreadCrumb.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from 'styled-components'
+import {ReactComponent as Backarrow} from './Vector.svg'
+import {makeStyles} from '@mui/styles'
+
+const useStyles = makeStyles({
+    backArrow:{
+        margin:"0px 50px",
+    }
+})
+
+function BreadCrumb() {
+
+    const classes = useStyles()
+
+    const BreadCrumb = styled.div`
+        width:100%;
+        text-align:left;
+        padding:2rem 0px;
+        display:flex;
+        flex-direction:row;
+    `
+
+    const Path = styled.div`
+        width:fit-content;
+    `
+
+    return (
+        <BreadCrumb>
+            <Backarrow className={classes.backArrow} />
+            <Path>
+                Car Interiors / Seat Cover / PU Leather / Product Name
+            </Path>
+
+        </BreadCrumb>
+    )
+}
+
+export default BreadCrumb
---
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BreadCrumb from './Breadcrumb/BreadCrumb'
+
+function ProductDetails() {
+    return (
+        <div>
+            <BreadCrumb></BreadCrumb>
+        </div>
+    )
+}
+
+export default ProductDetails
